8c23df1bd78346203c1797aa6e20d0be09929930
Ai Nguyen <ainguyenkaka@gmail.com>
2018-02-22 10:17:05 +0800

    expose service endpoints

--- src/main/java/com/sutd/statnlp/annotationimage/config/SwaggerConfig.java
+++ src/main/java/com/sutd/statnlp/annotationimage/config/SwaggerConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -20,9 +23,22 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo(applicationProperties.getSwagger()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant(applicationProperties.getSwagger().getDefaultIncludePattern()))
                 .build();
+    }
+
+    private ApiInfo apiInfo(ApplicationProperties.Swagger swagger) {
+        return new ApiInfo(
+                swagger.getTitle(),
+                swagger.getDescription(),
+                swagger.getVersion(),
+                null,
+                null,
+                null,
+                null,
+                Collections.emptyList());
     }
 }
--- src/main/java/com/sutd/statnlp/annotationimage/model/Annotation.java
+++ src/main/java/com/sutd/statnlp/annotationimage/model/Annotation.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import javax.validation.constraints.*;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 
 /**
